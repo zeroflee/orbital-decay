abc513eb5c2d67dcd34d384c4eab657fe52907a5
zeroflee <142126699+zeroflee@users.noreply.github.com>
2023-08-12 21:49:49 -0400

    Update module.js

--- scripts/module.js
+++ scripts/module.js
@@ -1,6 +1,32 @@
 'use strict';
 import {libWrapper} from './shim.js';
 
+//  is all junk from the module tutorial that can be stripped out later.
+class OrbitalDecay {
+  static ID = 'orbital-decay';
+
+  static FLAGS = {
+    TODOS: 'todos'
+  }
+
+  static TEMPLATES = {
+    TODOLIST: 'modules/${this.ID}/templates/orbital-decay.hbs'
+  }
+
+  static log(force, ...args) {
+    const shouldLog = force || game.modules.get('_dev-mode')?.api.getPackageDebugValue(this.ID);
+
+    if (shouldLog) {
+      console.log(this.ID, '|', ...args);
+    }
+  }
+}
+
+Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {
+  registerPackageDebugFlag(OrbitalDecay.ID);
+});
+  
+
 Hooks.once('init', async function() {
 
 });
